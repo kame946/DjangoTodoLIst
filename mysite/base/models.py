af0d92af86afa7b867e286b438971eaf89b89b61
kame946 <harshkumar7179@gmail.com>
2023-03-23 00:19:43 +0530

    first commit

---
+++ mysite/base/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    # Ordering of Data, viz. any complete model will be placed at the end of the list
+    class Meta:
+        ordering = ['complete']
